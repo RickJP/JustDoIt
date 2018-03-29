64c87dcbd9422df20449b751c5aa27a3cf7928db
Rick <rickd1977@gmail.com>
2018-03-29 11:56:49 +0900

    Convert to MVC design pattern

---
+++ JustDoIt/Data Model/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  JustDoIt
+//
+//  Created by Rick D on 2018/03/29.
+//  Copyright © 2018 Firefly. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+    
+}
